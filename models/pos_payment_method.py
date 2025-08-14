e702d2ac3e57c70f4f71fe0bdc2ee271525f6855
lfernandez <alejandroferllub@gmail.com>
2025-08-13 20:00:25 -0400

    [UPDATE] delete unnecessary files

--- models/pos_payment_method.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-
-class PosPaymentMethod(models.Model):
-    _inherit = 'pos.payment.method'
-
-    
-    is_credit_note = fields.Boolean(
-        string='Credit Note',
-    )
