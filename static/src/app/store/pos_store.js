1bfd6d7435ad0510ebd5a764354166753bf6fbb9
lfernandez <alejandroferllub@gmail.com>
2025-08-16 01:56:51 -0400

    [UPDATE] recibo de pos

--- static/src/app/store/pos_store.js
+++ static/src/app/store/pos_store.js
@@ -7,6 +7,8 @@ patch(PosStore.prototype, {
         headerData.pos_ncf = order.pos_ncf;
         headerData.partner = order.get_partner();
         headerData.pos_latam_document_type_name = order.pos_latam_document_type_name;
+        headerData.pos_date = order.date_order;
+        headerData.pos_reference = order.pos_reference;
         return headerData;
     }
 });
